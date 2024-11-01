62a993bb13229ced1521d69aad31bba47e957a08
Vincent Yang <missuo@pm.me>
2024-11-01 00:43:57 -0400

    fix: unable to translate

--- translate/types.go
+++ translate/types.go
@@ -1,8 +1,8 @@
 /*
  * @Author: Vincent Young
  * @Date: 2024-09-16 11:59:24
- * @LastEditors: Vincent Young
- * @LastEditTime: 2024-09-16 12:06:36
+ * @LastEditors: Vincent Yang
+ * @LastEditTime: 2024-11-01 00:39:49
  * @FilePath: /DeepLX/translate/types.go
  * @Telegram: https://t.me/missuo
  * @GitHub: https://github.com/missuo
@@ -12,31 +12,46 @@
 
 package translate
 
+// Lang represents the language settings for translation
 type Lang struct {
-	SourceLangUserSelected string `json:"source_lang_user_selected"`
-	TargetLang             string `json:"target_lang"`
+	SourceLangComputed string `json:"source_lang_computed,omitempty"`
+	TargetLang         string `json:"target_lang"`
+	LangUserSelected   string `json:"lang_user_selected,omitempty"`
 }
 
+// CommonJobParams represents common parameters for translation jobs
 type CommonJobParams struct {
-	WasSpoken       bool   `json:"wasSpoken"`
-	TranscribeAS    string `json:"transcribe_as"`
-	RegionalVariant string `json:"regionalVariant,omitempty"`
+	Mode string `json:"mode"`
 }
 
+// Sentence represents a sentence in the translation request
+type Sentence struct {
+	Prefix string `json:"prefix"`
+	Text   string `json:"text"`
+	ID     int    `json:"id"`
+}
+
+// Job represents a translation job
+type Job struct {
+	Kind               string     `json:"kind"`
+	PreferredNumBeams  int        `json:"preferred_num_beams"`
+	RawEnContextBefore []string   `json:"raw_en_context_before"`
+	RawEnContextAfter  []string   `json:"raw_en_context_after"`
+	Sentences          []Sentence `json:"sentences"`
+}
+
+// Params represents parameters for translation requests
 type Params struct {
-	Texts           []Text          `json:"texts"`
-	Splitting       string          `json:"splitting"`
-	Lang            Lang            `json:"lang"`
-	Timestamp       int64           `json:"timestamp"`
 	CommonJobParams CommonJobParams `json:"commonJobParams"`
-	TagHandling     string          `json:"tag_handling"`
+	Lang            Lang            `json:"lang"`
+	Texts           []string        `json:"texts,omitempty"`
+	TextType        string          `json:"textType,omitempty"`
+	Jobs            []Job           `json:"jobs,omitempty"`
+	Priority        int             `json:"priority,omitempty"`
+	Timestamp       int64           `json:"timestamp"`
 }
 
-type Text struct {
-	Text                string `json:"text"`
-	RequestAlternatives int    `json:"requestAlternatives"`
-}
-
+// PostData represents the complete translation request
 type PostData struct {
 	Jsonrpc string `json:"jsonrpc"`
 	Method  string `json:"method"`
@@ -44,26 +59,50 @@ type PostData struct {
 	Params  Params `json:"params"`
 }
 
-type Translation struct {
-	Text string `json:"text"`
+// SplitTextResponse represents the response from text splitting
+type SplitTextResponse struct {
+	Jsonrpc string `json:"jsonrpc"`
+	ID      int64  `json:"id"`
+	Result  struct {
+		Lang struct {
+			Detected string `json:"detected"`
+		} `json:"lang"`
+		Texts []struct {
+			Chunks []struct {
+				Sentences []struct {
+					Prefix string `json:"prefix"`
+					Text   string `json:"text"`
+				} `json:"sentences"`
+			} `json:"chunks"`
+		} `json:"texts"`
+	} `json:"result"`
 }
 
+// TranslationResponse represents the response from translation
 type TranslationResponse struct {
-	Translations []Translation `json:"translations"`
+	Jsonrpc string `json:"jsonrpc"`
+	ID      int64  `json:"id"`
+	Result  struct {
+		Translations []struct {
+			Beams []struct {
+				Sentences []struct {
+					Text string `json:"text"`
+				} `json:"sentences"`
+			} `json:"beams"`
+		} `json:"translations"`
+		SourceLang string `json:"source_lang"`
+		TargetLang string `json:"target_lang"`
+	} `json:"result"`
 }
 
-type DeepLUsageResponse struct {
-	CharacterCount int `json:"character_count"`
-	CharacterLimit int `json:"character_limit"`
-}
-
+// DeepLXTranslationResult represents the final translation result
 type DeepLXTranslationResult struct {
-	Code         int
-	ID           int64
-	Message      string
-	Data         string
-	Alternatives []string
-	SourceLang   string
-	TargetLang   string
-	Method       string
+	Code         int      `json:"code"`
+	ID           int64    `json:"id"`
+	Message      string   `json:"message,omitempty"`
+	Data         string   `json:"data"`
+	Alternatives []string `json:"alternatives"`
+	SourceLang   string   `json:"source_lang"`
+	TargetLang   string   `json:"target_lang"`
+	Method       string   `json:"method"`
 }
--- translate/utils.go
+++ translate/utils.go
@@ -1,8 +1,8 @@
 /*
  * @Author: Vincent Young
  * @Date: 2024-09-16 11:59:24
- * @LastEditors: Vincent Young
- * @LastEditTime: 2024-09-16 12:06:44
+ * @LastEditors: Vincent Yang
+ * @LastEditTime: 2024-11-01 00:39:32
  * @FilePath: /DeepLX/translate/utils.go
  * @Telegram: https://t.me/missuo
  * @GitHub: https://github.com/missuo
@@ -13,15 +13,18 @@
 package translate
 
 import (
+	"encoding/json"
 	"math/rand"
 	"strings"
 	"time"
 )
 
+// getICount returns the number of 'i' characters in the text
 func getICount(translateText string) int64 {
 	return int64(strings.Count(translateText, "i"))
 }
 
+// getRandomNumber generates a random number for request ID
 func getRandomNumber() int64 {
 	src := rand.NewSource(time.Now().UnixNano())
 	rng := rand.New(src)
@@ -29,12 +32,31 @@ func getRandomNumber() int64 {
 	return num * 1000
 }
 
+// getTimeStamp generates timestamp for request based on i count
 func getTimeStamp(iCount int64) int64 {
 	ts := time.Now().UnixMilli()
 	if iCount != 0 {
 		iCount = iCount + 1
 		return ts - ts%iCount + iCount
-	} else {
-		return ts
 	}
+	return ts
+}
+
+// formatPostString formats the request JSON string with specific spacing rules
+func formatPostString(postData *PostData) string {
+	postBytes, _ := json.Marshal(postData)
+	postStr := string(postBytes)
+
+	if (postData.ID+5)%29 == 0 || (postData.ID+3)%13 == 0 {
+		postStr = strings.Replace(postStr, `"method":"`, `"method" : "`, 1)
+	} else {
+		postStr = strings.Replace(postStr, `"method":"`, `"method": "`, 1)
+	}
+
+	return postStr
+}
+
+// isRichText checks if text contains HTML-like tags
+func isRichText(text string) bool {
+	return strings.Contains(text, "<") && strings.Contains(text, ">")
 }
